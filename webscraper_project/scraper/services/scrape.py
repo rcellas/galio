8d06036c59a038db509325529597e4209e08e46d
rcellas <rocio.cejudo.soronellas@gmail.com>
2025-03-13 11:33:49 +0100

    initial commit

---
+++ webscraper_project/scraper/services/scrape.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Para Chrome
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+def scrape_website():
+    # Configurar Selenium
+    options = Options()
+    options.add_argument('--headless')  # Ejecutar en modo headless
+    options.add_argument('--no-sandbox')  # Requerido para algunos servidores
+    options.add_argument('--disable-dev-shm-usage')  # Para evitar errores de memoria
+
+    # 🔹 Aquí inicializamos correctamente `service`
+    service = Service(ChromeDriverManager().install())
+
+    # Para Chrome
+    # Selenium Manager se encargará de descargar y gestionar el WebDriver
+    #service = Service()  # No es necesario especificar el ejecutable
+    driver = webdriver.Chrome(service=service, options=options)
+
+    # Navegar al sitio web
+    url = "https://jorgebenitezlopez.com"
+    driver.get(url)
+    print(driver.title)  
+# Esperar a que los elementos estén presentes
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, "h1"))
+        )
+        titles = driver.find_elements(By.CSS_SELECTOR, "h1")
+        urls = driver.find_elements(By.CSS_SELECTOR, "a")
+    except Exception as e:
+        print("Error al encontrar los elementos:", e)
+        driver.quit()
+        return []
+
+    scraped_data = []
+    for title, link in zip(titles, urls):
+        scraped_data.append({
+            "title": title.text,
+            "url": link.get_attribute("href"),
+        })
+
+    print("Scraped data:", scraped_data)  # Para depuración
+    driver.quit()
+    return scraped_data
