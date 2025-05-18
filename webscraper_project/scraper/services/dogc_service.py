7ecee6d76d8d3ac2ef25e6d04d036ac1e4e8ac4e
rcellas <rocio.cejudo.soronellas@gmail.com>
2025-05-18 13:52:37 +0200

    fix: dogc read title convo

--- webscraper_project/scraper/services/dogc_service.py
+++ webscraper_project/scraper/services/dogc_service.py
@@ -7,20 +7,22 @@ def scrape_dogc(driver, url):
     results = []
     try:
         WebDriverWait(driver, 10).until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, "div.download a"))
+            EC.presence_of_element_located((By.CLASS_NAME, "destacat_text_cont"))
         )
-        download_divs = driver.find_elements(By.CSS_SELECTOR, "div.download")
-        for div_download in download_divs:
+        items = driver.find_elements(By.CLASS_NAME, "destacat_text_cont")
+        for item in items:
             try:
-                a_tag_pdf = div_download.find_element(By.TAG_NAME, "a")
-                pdf_url = a_tag_pdf.get_attribute("href")
-                if pdf_url.startswith("/"):
-                    pdf_url = urljoin(url, pdf_url)
-                title = a_tag_pdf.text.strip()
+                # Título
+                title_elem = item.find_element(By.TAG_NAME, "a")
+                title = title_elem.text.strip()
+                link = urljoin(url, title_elem.get_attribute("href"))
+                # PDF
+                pdf_elem = item.find_element(By.CSS_SELECTOR, "div.download a")
+                pdf_url = pdf_elem.get_attribute("href")
                 results.append({
                     "url_base": url,
                     "title": title,
-                    "link": url,
+                    "link": link,
                     "pdf_url": pdf_url
                 })
             except Exception:
