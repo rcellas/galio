0ad7166d05e2b8dab13d31b2169eadf82af2c290
rcellas <rocio.cejudo.soronellas@gmail.com>
2025-05-29 00:57:10 +0200

    hotfix:error in entrypoint file

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,8 @@
 # Start cron in the background
 cron &
 
+# Change to the project directory
+cd /app/webscraper_project
+
 # Start Gunicorn in the foreground, binding to Render's $PORT
 gunicorn webscraper_project.wsgi:application --bind 0.0.0.0:$PORT
