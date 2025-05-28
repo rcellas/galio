88e6f1a46540196c5bf809a4478499941d383ad9
rcellas <rocio.cejudo.soronellas@gmail.com>
2025-05-29 01:15:16 +0200

    Add Django migrations to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,9 @@ cron &
 # Change to the project directory
 cd /app/webscraper_project
 
+# Run Django migrations to create database tables
+python manage.py makemigrations
+python manage.py migrate
+
 # Start Gunicorn in the foreground, binding to Render's $PORT
 gunicorn webscraper_project.wsgi:application --bind 0.0.0.0:$PORT
